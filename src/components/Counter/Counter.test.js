20fb868df22b67dc34a926f5c867b30552887d39
Ilya Sidorchik <ilya@sidorchik.ru>
2020-05-31 14:18:27 +0300

    Change tabs with two spaces in Counter test

--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -6,53 +6,53 @@ import { render, fireEvent } from '../../utils/test-utils';
 import ConnectedCounter from '.';
 
 describe('Counter', () => {
-	it('renders with redux with defaults', () => {
-		const { container } = render(<ConnectedCounter />);
-		const buttonIncrement = container.querySelector(
-			'.Counter-Button_increment'
-		);
-		const value = container.querySelector('.Counter-Value');
+  it('renders with redux with defaults', () => {
+    const { container } = render(<ConnectedCounter />);
+    const buttonIncrement = container.querySelector(
+      '.Counter-Button_increment'
+    );
+    const value = container.querySelector('.Counter-Value');
 
-		fireEvent.click(buttonIncrement);
+    fireEvent.click(buttonIncrement);
 
-		expect(value).toHaveTextContent('1');
-	});
+    expect(value).toHaveTextContent('1');
+  });
 
-	it('renders with redux with custom initial state', () => {
-		const { container } = render(<ConnectedCounter />, {
-			initialState: { ...rootPreloadedState, counter: { count: 3 } },
-		});
-		const buttonDecrement = container.querySelector(
-			'.Counter-Button_decrement'
-		);
-		const value = container.querySelector('.Counter-Value');
+  it('renders with redux with custom initial state', () => {
+    const { container } = render(<ConnectedCounter />, {
+      initialState: { ...rootPreloadedState, counter: { count: 3 } },
+    });
+    const buttonDecrement = container.querySelector(
+      '.Counter-Button_decrement'
+    );
+    const value = container.querySelector('.Counter-Value');
 
-		fireEvent.click(buttonDecrement);
+    fireEvent.click(buttonDecrement);
 
-		expect(value).toHaveTextContent('2');
-	});
+    expect(value).toHaveTextContent('2');
+  });
 
-	it('renders with redux with custom store', () => {
-		const store = createStore(() => ({
-			...rootPreloadedState,
-			counter: { count: 1000 },
-		}));
+  it('renders with redux with custom store', () => {
+    const store = createStore(() => ({
+      ...rootPreloadedState,
+      counter: { count: 1000 },
+    }));
 
-		const { container } = render(<ConnectedCounter />, {
-			store,
-		});
-		const buttonIncrement = container.querySelector(
-			'.Counter-Button_increment'
-		);
-		const buttonDecrement = container.querySelector(
-			'.Counter-Button_decrement'
-		);
-		const value = container.querySelector('.Counter-Value');
+    const { container } = render(<ConnectedCounter />, {
+      store,
+    });
+    const buttonIncrement = container.querySelector(
+      '.Counter-Button_increment'
+    );
+    const buttonDecrement = container.querySelector(
+      '.Counter-Button_decrement'
+    );
+    const value = container.querySelector('.Counter-Value');
 
-		fireEvent.click(buttonIncrement);
-		expect(value).toHaveTextContent('1000');
+    fireEvent.click(buttonIncrement);
+    expect(value).toHaveTextContent('1000');
 
-		fireEvent.click(buttonDecrement);
-		expect(value).toHaveTextContent('1000');
-	});
+    fireEvent.click(buttonDecrement);
+    expect(value).toHaveTextContent('1000');
+  });
 });
